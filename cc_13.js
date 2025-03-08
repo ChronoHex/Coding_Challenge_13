30c758de6b3bd93ba92050b3a1e843b0c48dab50
Eugene Rodriguez <er11@usf.edu>
2025-03-07 22:36:34 -0500

    Task 2 - Employee Cards Dynamic Addition

---
+++ cc_13.js
@@ -0,0 +1,33 @@
+// task 2
+function addEmployeeCard(name, position) {
+
+    const card = document.createElement('div');
+    card.setAttribute('class', 'employee-card');
+
+    const heading = document.createElement('h3');
+    heading.textContent = name;
+
+    const paragragh = document.createElement('h3');
+    paragragh.textContent = position;
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+
+    // appending elements to the card
+    card.appendChild(heading);
+    card.appendChild(paragragh);
+    card.appendChild(removeButton);
+
+    // appending card to employee container
+    document.getElementById('employeeContainer').appendChild(card);
+
+    // remove functionality
+    removeButton.addEventListener('click', function() {
+        card.remove();
+    });
+};
+
+// example employees
+addEmployeeCard('Ed', 'Tester');
+addEmployeeCard('Edd', 'Diagnostics');
+addEmployeeCard('Eddy', 'Manager');
